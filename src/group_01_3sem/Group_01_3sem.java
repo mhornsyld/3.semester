0ee7c419b4fd7dada79ffba1eae8d080b1d0fb9e
RasmusRichter <RasmusRichter@10.18.132.29>
2014-02-14 11:59:51 +0100

    SLET!

--- src/group_01_3sem/Group_01_3sem.java
+++ src/group_01_3sem/Group_01_3sem.java
@@ -13,7 +13,6 @@ public class Group_01_3sem {
      */
     public static void main(String[] args) {
         System.out.println("Pedro is sooooo gay");
-        System.out.println("Noobs.");
         if(new Random().nextInt(2) == 1)
         {
             System.out.println("YEAH!!!");
