752de284d7a71e5f7e5f222277b0f4548e62ffe1
Jacob Nicolai Ege Sørensen <jacob.ege@gmail.com>
2014-02-19 09:41:35 +0100

    Added Person interface

---
+++ src/Interface/Person.java
@@ -0,0 +1,14 @@
+package Interface;
+
+/**
+ *
+ * @author Jacob
+ */
+public interface Person {
+    public int getSocialSecurityNumber();
+    public String getFullName();
+    public String getTitle();
+    public String getPhoneNumber();
+    public String getEmail();
+    public String getAddress();
+}
